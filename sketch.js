1553417601af1191084b351cc7db350ead2306de
MarioSal13 <mariosalmartinezren@gmail.com>
2025-04-10 09:29:44 -0700

    Nuevo algortimo

--- sketch.js
+++ sketch.js
@@ -22,17 +22,76 @@ function setup() {
 function drawPizzas() {
   background(255);
   numSlices = int(input.value());
-
   if (numSlices <= 0) return;
 
-  drawPizza(150, 200, 100, numSlices, drawLinePointSlope, 'red', "Punto-Pendiente");
+
+  drawPizzaMidpoint(150, 200, 100, numSlices, 'red', "Punto Medio");
+
+
   drawPizza(370, 200, 100, numSlices, drawLineDDA, 'blue', "DDA");
+
+
   drawPizza(590, 200, 100, numSlices, drawLineBresenham, 'green', "Bresenham");
 }
+
+function drawPizzaMidpoint(cx, cy, r, slices, color, label) {
+  stroke(color);
+  fill(255);
+
+
+  drawCircleMidpoint(cx, cy, r);
+
+  for (let i = 0; i < slices; i++) {
+    let angle = TWO_PI * i / slices;
+    let x2 = cx + r * cos(angle);
+    let y2 = cy + r * sin(angle);
+    drawLinePointSlope(cx, cy, x2, y2);
+  }
+
+  fill(0);
+  noStroke();
+  text(label, cx, cy + r + 20);
+}
+
+function drawCircleMidpoint(cx, cy, r) {
+  strokeWeight(1);
+
+  let x = 0;
+  let y = r;
+
+  let p = 1 - r;
+
+  plotCirclePoints(cx, cy, x, y);
+
+  while (x < y) {
+    x++;
+    if (p < 0) {
+      p += 2 * x + 1;
+    } else {
+      y--;
+      p += 2 * (x - y) + 1;
+    }
+    plotCirclePoints(cx, cy, x, y);
+  }
+}
+
+function plotCirclePoints(cx, cy, x, y) {
+  point(cx + x, cy + y);
+  point(cx - x, cy + y);
+  point(cx + x, cy - y);
+  point(cx - x, cy - y);
+
+  point(cx + y, cy + x);
+  point(cx - y, cy + x);
+  point(cx + y, cy - x);
+  point(cx - y, cy - x);
+}
+
 
 function drawPizza(cx, cy, radius, slices, drawFn, color, label) {
   stroke(color);
   fill(255);
+
   ellipse(cx, cy, radius * 2, radius * 2);
 
   for (let i = 0; i < slices; i++) {
@@ -46,6 +105,7 @@ function drawPizza(cx, cy, radius, slices, drawFn, color, label) {
   noStroke();
   text(label, cx, cy + radius + 20);
 }
+
 
 function drawLinePointSlope(x1, y1, x2, y2) {
   strokeWeight(1);
@@ -76,6 +136,7 @@ function drawLinePointSlope(x1, y1, x2, y2) {
 
 function drawLineDDA(x1, y1, x2, y2) {
   strokeWeight(1);
+
   let dx = x2 - x1;
   let dy = y2 - y1;
   let steps = max(abs(dx), abs(dy));
@@ -93,11 +154,16 @@ function drawLineDDA(x1, y1, x2, y2) {
 
 function drawLineBresenham(x1, y1, x2, y2) {
   strokeWeight(1);
-  x1 = int(x1); y1 = int(y1); x2 = int(x2); y2 = int(y2);
+
+  x1 = int(x1); 
+  y1 = int(y1); 
+  x2 = int(x2); 
+  y2 = int(y2);
+
   let dx = abs(x2 - x1);
   let dy = abs(y2 - y1);
-  let sx = x1 < x2 ? 1 : -1;
-  let sy = y1 < y2 ? 1 : -1;
+  let sx = (x1 < x2) ? 1 : -1;
+  let sy = (y1 < y2) ? 1 : -1;
   let err = dx - dy;
 
   while (true) {
